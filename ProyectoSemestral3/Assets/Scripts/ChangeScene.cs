1029bc292fbc949bc8fae67c6a69baa8a15cbf55
Amerike <miguelsantiagoews@gmail.com>
2022-03-07 07:47:00 -0600

    Add space

--- ProyectoSemestral3/Assets/Scripts/ChangeScene.cs
+++ ProyectoSemestral3/Assets/Scripts/ChangeScene.cs
@@ -4,6 +4,7 @@ using UnityEngine.SceneManagement;
 public class ChangeScene : MonoBehaviour
 {
     [SerializeField] GameObject _menu = default;
+
     public void MoveToscene(int sceneID)
     {
         SceneManager.LoadScene(sceneID);
