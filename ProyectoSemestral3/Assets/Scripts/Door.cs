02e757a5eb082263a1cbb37b97253827c6cb5951
Miguel Alejandro Santiago Perez <miguelsantiagoews@gmail.com>
2022-04-02 17:31:47 -0600

    AddedAssets and build map

--- ProyectoSemestral3/Assets/Scripts/Door.cs
+++ ProyectoSemestral3/Assets/Scripts/Door.cs
@@ -3,18 +3,11 @@ using UnityEngine;
 public class Door : MonoBehaviour
 {
     [SerializeField] private BoxCollider _doorCollider = default;
-    [SerializeField] private GameObject _doorMenu = default;
 
     private void OnTriggerEnter(Collider other)
     {
         other = _doorCollider;
-        _doorMenu.SetActive(true);
         InventoryManager.Instance.FindKey();
-    }
-
-    private void OnTriggerExit(Collider other)
-    {
-        _doorMenu.SetActive(false);
     }
 }
 
